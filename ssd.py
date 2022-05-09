3add1bd22a4b943d2c23898d8bbea0187dec704f
Kelvin <47792299+kelvincrdz@users.noreply.github.com>
2022-05-09 09:27:17 -0300

    Update ssd.py

--- ssd.py
+++ ssd.py
@@ -6,127 +6,112 @@ Updated by: F Kelvin C Belo
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# handbook
-# from torch.autograd import Variable
-# handbook
+
 from layers import *
 from data import voc, coco
 import os
 
 
 class SSD(nn.Module):
-    """Single Shot Multibox Architecture
-    The network is composed of a base VGG network followed by the
-    added multibox conv layers.  Each multibox layer branches into
-        1) conv2d for class conf scores
-        2) conv2d for localization predictions
-        3) associated priorbox layer to produce default bounding
-           boxes specific to the layer's feature map size.
-    See: https://arxiv.org/pdf/1512.02325.pdf for more details.
+    """Arquitetura de caixa múltipla de disparo único
+     A rede é composta por uma rede VGG básica seguida pela rede
+     camadas de conversão de caixa múltipla adicionadas. Cada camada multibox se ramifica em
+         1) conv2d para pontuações de configuração de classe
+         2) conv2d para previsões de localização
+         3) camada de caixa anterior associada para produzir delimitação padrão
+            caixas específicas para o tamanho do mapa de feição da camada.
+     Veja: https://arxiv.org/pdf/1512.02325.pdf para mais detalhes.
 
-    Args:
-        phase: (string) Can be "test" or "train"
-        size: input image size
-        base: VGG16 layers for input, size of either 300 or 500
-        extras: extra layers that feed to multibox loc and conf layers
-        head: "multibox head" consists of loc and conf conv layers
-    """
-
+     Args:
+         phase: (string) Pode ser "teste" ou "treinar"
+         tamanho: tamanho da imagem de entrada
+         base: camadas VGG16 para entrada, tamanho de 300 ou 500
+         extras: camadas extras que alimentam as camadas multibox loc e conf
+         head: "multibox head" consiste em camadas conv loc e conf
+     """
     def __init__(self, phase, size, base, extras, head, num_classes):
         super(SSD, self).__init__()
         self.phase = phase
         self.num_classes = num_classes
         self.cfg = (coco, voc)[num_classes == 21]
         self.priorbox = PriorBox(self.cfg)
-        # handbook
-        # self.priors = Variable(self.priorbox.forward(), volatile=True)
-        self.priors = self.priorbox.forward()
-        # handbook
+        self.priors = self.priorbox.forward() 
         self.size = size
 
-        # SSD network
+
         self.vgg = nn.ModuleList(base)
-        # Layer learns to scale the l2 normalized features from conv4_3
+
         self.L2Norm = L2Norm(512, 20)
         self.extras = nn.ModuleList(extras)
-        # オフセットと確信度のネットワークリスト
+
         self.loc = nn.ModuleList(head[0])
         self.conf = nn.ModuleList(head[1])
 
-        # demo実行時
+
         if phase == 'test':
             self.softmax = nn.Softmax(dim=-1)
-            # PyTorch1.5.0 support new-style autograd function
-            #self.detect = Detect(num_classes, 0, 200, 0.01, 0.45)
+
             self.detect = Detect()
-            # PyTorch1.5.0 support new-style autograd function
 
-    # 順伝播
+
+
     def forward(self, x):
-        """Applies network layers and ops on input image(s) x.
+       """Aplica camadas de rede e operações na(s) imagem(ns) de entrada x.
 
-        Args:
-            x: input image or batch of images. Shape: [batch,3,300,300].
+         Args:
+             x: imagem de entrada ou lote de imagens. Forma: [lote, 3.300.300].
 
-        Return:
-            Depending on phase:
-            test:
-                Variable(tensor) of output class label predictions,
-                confidence score, and corresponding location predictions for
-                each object detected. Shape: [batch,topk,7]
+         Retornar:
+             Dependendo da fase:
+             teste:
+                 Variável (tensor) de previsões de rótulo de classe de saída,
+                 pontuação de confiança e previsões de localização correspondentes para
+                 cada objeto detectado. Forma: [lote, topk, 7]
 
-            train:
-                list of concat outputs from:
-                    1: confidence layers, Shape: [batch*num_priors,num_classes]
-                    2: localization layers, Shape: [batch,num_priors*4]
-                    3: priorbox layers, Shape: [2,num_priors*4]
-        """
+             Comboio:
+                 lista de saídas concat de:
+                     1: camadas de confiança, Forma: [lote*num_priors,num_classes]
+                     2: camadas de localização, Forma: [lote,num_priors*4]
+                     3: camadas da caixa anterior, Forma: [2,num_priors*4]
+         """
         sources = list()
         loc = list()
         conf = list()
 
-        # apply vgg up to conv4_3 relu
+
         for k in range(23):
             x = self.vgg[k](x)
-        # Conv4-3>Reluの計算結果にL2Normを適用しsourcesに追加
+
         s = self.L2Norm(x)
         sources.append(s)
 
-        # apply vgg up to fc7
         for k in range(23, len(self.vgg)):
             x = self.vgg[k](x)
-        # Conv7>Reluの計算結果をsourcesに追加
+
         sources.append(x)
 
-        # 追加ネットワークにrelu関数を追加し順伝播
-        # 奇数番目の層の計算結果をsourcesに追加
-        # apply extra layers and cache source layer outputs
         for k, v in enumerate(self.extras):
             x = F.relu(v(x), inplace=True)
             if k % 2 == 1:
                 sources.append(x)
 
-        # apply multibox head to source layers
         for (x, l, c) in zip(sources, self.loc, self.conf):
-            # (バッチサイズ,C,W,H) → (バッチサイズ,W,H,C)にTranspose
             loc.append(l(x).permute(0, 2, 3, 1).contiguous())
             conf.append(c(x).permute(0, 2, 3, 1).contiguous())
 
         loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
         conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
-        # demo実行時
         if self.phase == "test":
-            # PyTorch1.5.0 support new-style autograd function
-            #output = self.detect(
+
             output = self.detect.apply(self.num_classes, 0, 200, 0.01, 0.45,
-            # PyTorch1.5.0 support new-style autograd function
+
                 loc.view(loc.size(0), -1, 4),                   # loc preds
                 self.softmax(conf.view(conf.size(0), -1,
                              self.num_classes)),                # conf preds
                 self.priors.type(type(x.data))                  # default boxes
             )
         else:
-        # train実行時
+        # train
             output = (
                 loc.view(loc.size(0), -1, 4),
                 conf.view(conf.size(0), -1, self.num_classes),
@@ -145,17 +130,14 @@ class SSD(nn.Module):
             print('Sorry only .pth and .pkl files supported.')
 
 
-# This function is derived from torchvision VGG make_layers()
-# https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
-# ベースネットワークのリスト作成
 def vgg(cfg, i, batch_norm=False):
     layers = []
     in_channels = i
     for v in cfg:
-        # プーリング層　300×300　→　150×150
+
         if v == 'M':
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        # プーリング層で小数点切り上げ　75×75 →　38×38
+
         elif v == 'C':
             layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
         else:
@@ -173,16 +155,15 @@ def vgg(cfg, i, batch_norm=False):
     return layers
 
 
-# 追加ネットワークのリスト作成
 def add_extras(cfg, i, batch_norm=False):
-    # Extra layers added to VGG for feature scaling
+
     layers = []
     in_channels = i
     flag = False
     for k, v in enumerate(cfg):
         if in_channels != 'S':
             if v == 'S':
-                # strideが2
+
                 layers += [nn.Conv2d(in_channels, cfg[k + 1],
                            kernel_size=(1, 3)[flag], stride=2, padding=1)]
             else:
@@ -191,30 +172,30 @@ def add_extras(cfg, i, batch_norm=False):
         in_channels = v
     return layers
 
-# オフセット、確信度のネットワークのリスト作成
+
 def multibox(vgg, extra_layers, cfg, num_classes):
     loc_layers = []
     conf_layers = []
     vgg_source = [21, -2]
-    # ベースの21のConv4-3と-2(最後から2番目)のConv7を特徴マップのリストに追加
+
     for k, v in enumerate(vgg_source):
-        # 出力層の数はアスペクト比の数×座標数
+
         loc_layers += [nn.Conv2d(vgg[v].out_channels,
                                  cfg[k] * 4, kernel_size=3, padding=1)]
-        # 出力層の数はアスペクト比の数×クラス数
+
         conf_layers += [nn.Conv2d(vgg[v].out_channels,
                         cfg[k] * num_classes, kernel_size=3, padding=1)]
-    # 追加ネットの内、奇数番目の層を特徴マップのリストに追加
+
     for k, v in enumerate(extra_layers[1::2], 2):
-        # 出力層の数はアスペクト比の数×座標数
+
         loc_layers += [nn.Conv2d(v.out_channels, cfg[k]
                                  * 4, kernel_size=3, padding=1)]
-        # 出力層の数はアスペクト比の数×クラス数
+
         conf_layers += [nn.Conv2d(v.out_channels, cfg[k]
                                   * num_classes, kernel_size=3, padding=1)]
     return vgg, extra_layers, (loc_layers, conf_layers)
 
-# 数字は入力チャンネル、M,Cはプーリング、Sはstride=2
+
 base = {
     '300': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
             512, 512, 512],
@@ -224,13 +205,13 @@ extras = {
     '300': [256, 'S', 512, 128, 'S', 256, 128, 256, 128, 256],
     '512': [],
 }
-# 特徴マップ毎のアスペクト比の数
+
 mbox = {
     '300': [4, 6, 6, 6, 4, 4],  # number of boxes per feature map location
     '512': [],
 }
 
-# ネットワークのリスト作成
+
 def build_ssd(phase, size=300, num_classes=21):
     if phase != "test" and phase != "train":
         print("ERROR: Phase: " + phase + " not recognized")
@@ -239,7 +220,7 @@ def build_ssd(phase, size=300, num_classes=21):
         print("ERROR: You specified size " + repr(size) + ". However, " +
               "currently only SSD300 (size=300) is supported!")
         return
-    # ベース、追加、オフセット、確信度のネットワークリストはクラスSSDの引数
+
     base_, extras_, head_ = multibox(vgg(base[str(size)], 3),
                                      add_extras(extras[str(size)], 1024),
                                      mbox[str(size)], num_classes)
